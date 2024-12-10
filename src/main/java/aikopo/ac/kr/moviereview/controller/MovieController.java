b99e590fe3dfa184329b695eb269d1a951a77353
00seungho <tmdgh9910@naver.com>
2024-12-10 15:37:31 +0900

    완성

---
+++ src/main/java/aikopo/ac/kr/moviereview/controller/MovieController.java
@@ -0,0 +1,67 @@
+package aikopo.ac.kr.moviereview.controller;
+
+
+import aikopo.ac.kr.moviereview.dto.MovieDTO;
+import aikopo.ac.kr.moviereview.dto.PageRequestDTO;
+import aikopo.ac.kr.moviereview.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/movie")
+@Log4j2
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService movieService; //final
+
+    @GetMapping("/register")
+    public void register(){
+
+    }
+
+    @PostMapping("/register")
+    public String register(MovieDTO movieDTO, RedirectAttributes redirectAttributes){
+        log.info("movieDTO: " + movieDTO);
+
+        Long mno = movieService.register(movieDTO);
+
+        redirectAttributes.addFlashAttribute("msg", mno);
+
+        return "redirect:/movie/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        log.info("pageRequestDTO: " + pageRequestDTO);
+
+
+        model.addAttribute("result", movieService.getList(pageRequestDTO));
+
+    }
+
+    @GetMapping({"/read", "/modify"})
+    public void read(long mno,long page, @ModelAttribute("requestDTO") MovieDTO requestDTO, Model model ){
+
+        log.info("mno: " + mno);
+
+        MovieDTO movieDTO = movieService.getMovie(mno);
+
+        model.addAttribute("dto", movieDTO);
+        model.addAttribute("page", page);
+    }
+
+
+}
